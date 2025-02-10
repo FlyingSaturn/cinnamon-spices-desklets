f68e353d55b9817076a15b6f8af0b9d5b6e1d652
FlyingSaturn <pramanikba45@gmail.com>
2025-02-11 04:23:10 +0530

    Create Boilerplate and Metadata

--- daypercentagebot@flyingsaturn/files/daypercentagebot@flyingsaturn/desklet.js
+++ daypercentagebot@flyingsaturn/files/daypercentagebot@flyingsaturn/desklet.js
@@ -1,1 +1,21 @@
-function calculatePercentage()
+const Applet = imports.ui.applet;
+const Util = imports.misc.util;
+
+function MyApplet(orientation, panel_height, instance_id) {
+    this._init(orientation, panel_height, instance_id);
+}
+
+MyApplet.prototype = {
+    __proto__: Applet.IconApplet.prototype,
+
+    _init: function(orientation, panel_height, instance_id) {
+    },
+
+    on_applet_clicked: function(){
+    }
+
+};
+
+function main(metadata, orientation, panel_height, instance_id) {
+    return new MyApplet(orientation, panel_height, instance_id);
+}
